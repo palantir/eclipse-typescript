d3e9c112eddd368f6793ba7f482a2259d4e701f1
Derek Cicerone <dcicerone@palantir.com>
2013-06-12 16:14:24 -0400

    Fix warnings

--- TypeScript/src/com/palantir/typescript/editors/ColorManager.java
+++ TypeScript/src/com/palantir/typescript/editors/ColorManager.java
@@ -10,10 +10,10 @@ import org.eclipse.swt.widgets.Display;
 
 public class ColorManager {
 
-    protected Map fColorTable = new HashMap(10);
+    private Map<RGB, Color> fColorTable = new HashMap<>(10);
 
     public void dispose() {
-        Iterator e = fColorTable.values().iterator();
+        Iterator<Color> e = fColorTable.values().iterator();
         while (e.hasNext())
              ((Color) e.next()).dispose();
     }
