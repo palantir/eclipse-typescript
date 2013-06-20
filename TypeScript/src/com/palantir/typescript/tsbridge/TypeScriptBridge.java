c6d1c287a93872acfdf442966fb7047c54975ef5
tylera <tylera@palantir.com>
2013-06-19 22:33:02 -0400

    Moved default node location

--- TypeScript/src/com/palantir/typescript/tsbridge/TypeScriptBridge.java
+++ TypeScript/src/com/palantir/typescript/tsbridge/TypeScriptBridge.java
@@ -38,7 +38,7 @@ public final class TypeScriptBridge {
 
     private static TypeScriptBridge BRIDGE = null;
 
-    private static final String DEFAULT_NODE_LOCATION = "/opt/local/bin/node";
+    private static final String DEFAULT_NODE_LOCATION = "/usr/local/bin/node";
     private static final String DEFAULT_BRIDGE_LOCATION = "TSBridge/bin/bridge.js";
 
     private final BufferedReader fromServer;
