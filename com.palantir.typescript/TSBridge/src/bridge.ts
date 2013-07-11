6921ebdc7c92270cccddba380a01f28abca6ccfc
tylera <tylera@palantir.com>
2013-07-11 16:07:55 -0400

    Upgraded bridge.ts for new protocol (Syntax Highlight only).  Removed manager.ts

--- com.palantir.typescript/TSBridge/src/bridge.ts
+++ com.palantir.typescript/TSBridge/src/bridge.ts
@@ -15,249 +15,58 @@
  */
 
 ///<reference path='map.ts' />
-///<reference path='manager.ts' />
 ///<reference path='../lib/typescript/src/services/classifier.ts' />
-///<reference path='../lib/typescript/src/compiler/typescript.ts' />
-///<reference path='../lib/typescript/src/services/classifier.ts' />
-///<reference path='../lib/typescript/src/compiler/typescript.ts' />
-///<reference path='../lib/typescript/src/compiler/io.ts'/>
-///<reference path='../lib/typescript/src/compiler/typescript.ts'/>
 ///<reference path='../lib/typescript/src/services/languageService.ts' />
-///<reference path='../lib/typescript/src/services/shims.ts' />
-///<reference path='../lib/typescript/src/services/typescriptServices.ts' />
-///<reference path='../lib/typescript/src/services/diagnosticServices.ts' />
 
-/**
-  * This module handles everything related to syntax highlighting.  SyntaxHighlight.Service is the actual IService and it also comes with its own set of requests and results.
-  *
-  * @author tyleradams
-  */
-module SyntaxHighlight {
 
-    interface TokenWrapper {
-        tokenID: number;
-        offset: number;
-        length: number;
-    }
+module BridgeService {
 
-    interface ISyntaxHighlightRequest extends TypeScriptServiceBridge.IRequest {
-        offset: number;
-        documentText: string;
-    }
+    export class ClassifierService implements TypeScriptServiceBridge.IService {
 
-    interface ISyntaxHighlightResult extends TypeScriptServiceBridge.IResult {
-        tokenWrappers: TokenWrapper[];
-    }
-
-    /**
-      * This class handles syntax highlighting typed requests.  Currently it only handles the command syntax highlight.
-      */
-    export class Service implements TypeScriptServiceBridge.IService {
-
-        public getServiceType(): string {
-            return "syntax highlight";
-        }
-
-        public processRequest(request: TypeScriptServiceBridge.IRequest): TypeScriptServiceBridge.IResult {
-            if (request.command === "syntax highlight") {
-                return this.syntaxHighlight(<ISyntaxHighlightRequest> request);
-            } else {
-                return this.invalidCommand();
-            }
-        }
-
-        private syntaxHighlight(request: ISyntaxHighlightRequest): ISyntaxHighlightResult {
-            var result: ISyntaxHighlightResult;
-            result = {"resultValid": true, "resultType": request.command, "tokenWrappers": []}
-            var token: TokenWrapper;
-            var tokenArray: TokenWrapper[] = [];
-            var newline = "\r\n";
-            var classHost = new TypeScript.NullLogger();
-            var classifier = new Services.Classifier(classHost);
-            var contentsByLine = request.documentText.split(newline);
-            var lineState = Services.EndOfLineState.Start;
-            var classResults: Services.ClassificationResult[] = [];
-            var currentResult: Services.ClassificationResult;
-            var currentEntry: Services.ClassificationInfo;
-            var offset = request.offset;
-            for (var i=0; i<contentsByLine.length; i++) {
-                currentResult = classifier.getClassificationsForLine(contentsByLine[i], lineState);
-                for (var j=0; j<currentResult.entries.length; j++) {
-                    currentEntry = currentResult.entries[j];
-                    token = {"tokenID": currentEntry.classification, "offset": offset, "length": currentEntry.length};
-                    tokenArray.push(token);
-                    offset += currentEntry.length;
-                    lineState = currentResult.finalLexState;
-                }
-                offset += 2; // Eclipse counts the \r\n even though this method doesn't.  Here we compensate for that discrepency.
-            }
-            result.tokenWrappers = tokenArray;
-            return result;
-        }
-
-        private invalidCommand(): TypeScriptServiceBridge.IResult {
-            return TypeScriptServiceBridge.TSServiceBridge.invalidResult("A Request was made to Syntax Highlight, but it was bad.");
-        }
-    }
-}
-
-/**
-  * This module handles everything related to syntax highlighting.  SyntaxHighlight.Service is the actual IService and it also comes with its own set of requests and results.
-  *
-  * @author tyleradams
-  */
-module AutoComplete {
-
-    interface IAddFileRequest extends TypeScriptServiceBridge.IRequest {
-        fileName: string;
-        userRoot: string;
-    }
-
-    interface IAddFileWithReferencesRequest extends IAddFileRequest {
-    }
-
-    interface ICheckFileRequest extends TypeScriptServiceBridge.IRequest {
-        fileName: string;
-    }
-
-    interface IRemoveFileRequest extends TypeScriptServiceBridge.IRequest {
-        fileName: string;
-    }
-
-    interface IUpdateFileRequest extends TypeScriptServiceBridge.IRequest {
-        fileName: string;
-        content: string;
-    }
-
-    interface IAutoCompleteRequest extends TypeScriptServiceBridge.IRequest {
-        fileName: string;
-        offset: number;
-        isMemberCompletion: boolean;
-    }
-
-    interface ICheckFileResult extends TypeScriptServiceBridge.IResult {
-        fileLoaded: boolean;
-    }
-
-    interface IAutoCompleteResult extends TypeScriptServiceBridge.IResult {
-        autoCompletionInfo: AutoCompleteLibrary.IAutoCompletionInfo;
-    }
-
-    interface IDetailedAutoCompleteResult extends TypeScriptServiceBridge.IResult {
-        autoCompletionInfo: AutoCompleteLibrary.IDetailedAutoCompletionInfo;
-    }
-
-    /**
-      This is the AutoComplete Service class.  It provides methods which are necessary for mananging autocompletion.
-    */
-    export class Service implements TypeScriptServiceBridge.IService {
-
-        private manager: AutoCompleteLibrary.Manager;
+        private Classifier: Classifier;
 
         constructor() {
-            this.manager = new AutoCompleteLibrary.Manager();
+            this.Classifier = new Classifier();
         }
 
-        public getServiceType(): string {
-            return "Auto Complete";
+        public getServiceName(): string {
+            return "Classifier";
         }
 
-        public processRequest(request: TypeScriptServiceBridge.IRequest): TypeScriptServiceBridge.IResult {
-            if (request.command === "auto complete") {
-                return this.autoComplete(<IAutoCompleteRequest> request);
-            } else if (request.command === "add file") {
-                return this.addFile(<IAddFileRequest> request);
-            } else if (request.command === "add file with references") {
-                return this.addFileWithReferences(<IAddFileWithReferencesRequest> request);
-            } else if (request.command === "check file") {
-                return this.checkFile(<ICheckFileRequest> request);
-            } else if (request.command === "remove file") {
-                return this.removeFile(<IRemoveFileRequest> request);
-            } else if (request.command === "update file") {
-                return this.updateFile(<IUpdateFileRequest> request);
-            } else {
-                return this.invalidCommand(request);
-            }
-        }
-
-        private autoComplete(request: IAutoCompleteRequest): IAutoCompleteResult {
-            var file = request.fileName;
-            var position = request.offset;
-            var isMemberCompletion = request.isMemberCompletion;
-            var rawResult: AutoCompleteLibrary.IDetailedAutoCompletionInfo = this.manager.getDetailedImplicitPrunedCompletionsAtPosition(file, position, isMemberCompletion);
-            var result = {"resultType": request.command, "resultValid": true, autoCompletionInfo: rawResult};
-            return result;
-        }
-
-        private addFile(request: IAddFileRequest): TypeScriptServiceBridge.IResult {
-            var file = request.fileName;
-            var root = request.userRoot;
-            var rawResult: boolean = this.manager.addFile(file, root);
-            var result = {"resultType": request.command, "resultValid": rawResult};
-            if (result.resultValid === false) {
-                var eResult: TypeScriptServiceBridge.IError = {"resultType": request.command, "resultValid": rawResult, "errorMessage": "File cannot be added (probably because it has already been added)"};
-                return eResult;
-            }
-            return result;
-        }
-
-        private addFileWithReferences(request: IAddFileWithReferencesRequest): TypeScriptServiceBridge.IResult {
-            var fileName = request.fileName;
-            var root = request.userRoot;
-            this.manager.updateFile(fileName,root);
-            var filePath = request.userRoot + request.fileName;
-            var rawSource: string = IO.readFile(filePath).contents();
-            var fileSource: TypeScript.IScriptSnapshot = TypeScript.ScriptSnapshot.fromString(rawSource);
-            var referencedFilesRaw = TypeScript.getReferencedFiles(fileName, fileSource);
-            var numOfReferences: number = referencedFilesRaw.length;
-            var referencedFile: string;
-            var localRoot: string;
-            for (var i=0; i<numOfReferences; i++) {
-                referencedFile = referencedFilesRaw[i].path;
-                if (referencedFile.length > 0 && referencedFile[0] === "/") { // absolute path
-                    localRoot = "";
-                } else { // local path
-                    localRoot = root;
-                }
-                this.manager.updateFile(referencedFile, localRoot);
-            }
-            var result = {"resultValid": true, "resultType": request.command};
-            return result;
-        }
-
-        private checkFile(request: ICheckFileRequest): ICheckFileResult {
-            var file: string = request.fileName;
-            var fileLoaded: boolean = this.manager.checkFile(file);
-            var result = {"resultType": request.command, "resultValid": true, "fileLoaded": fileLoaded};
-            return result;
-        }
-
-        private updateFile(request: IUpdateFileRequest): TypeScriptServiceBridge.IResult {
-            var file = request.fileName;
-            var content = request.content;
-            var rawResult: boolean = this.manager.updateScript(file, content);
-            var result = {"resultType": request.command, "resultValid": rawResult};
-            return result;
-        }
-
-        private removeFile(request: IRemoveFileRequest): TypeScriptServiceBridge.IResult {
-            var file = request.fileName;
-            var rawResult: boolean = this.manager.removeFile(file);
-            var result = {"resultType": request.command, "resultValid": rawResult};
-            if (result.resultValid === false) {
-                var errorMessage = "File cannot be removed (probably because it already has been removed or was never added)";
-                var eResult: TypeScriptServiceBridge.IError = {"resultType": request.command, "resultValid": rawResult, "errorMessage": errorMessage};
-                return eResult;
-            }
-            return result;
-        }
-
-        private invalidCommand(request: TypeScriptServiceBridge.IRequest): TypeScriptServiceBridge.IResult {
-            return TypeScriptServiceBridge.TSServiceBridge.invalidResult("It turns out the command: \"" + request.command + "\" is a Bad Auto Complete Request");
+        public getService() {
+            return this.Classifier;
         }
     }
-}
 
+    export class Classifier {
+
+        private classifier: Services.Classifier;
+
+        constructor() {
+            this.classifier = new Services.Classifier(new TypeScript.NullLogger());
+        }
+
+        public getClassificationsForLines(lines: string[], lexState: Services.EndOfLineState) {
+            var line: string;
+            var localLexState: Services.EndOfLineState = lexState;
+            var classificationResult: Services.ClassificationResult;
+            var results: Services.ClassificationResult[] = [];
+
+            for (var i = 0; i < lines.length; i++) {
+                line = lines[i];
+                classificationResult = this.classifier.getClassificationsForLine(line, localLexState);
+                results.push(classificationResult);
+                localLexState = classificationResult.finalLexState;
+            }
+            var result: ClassificationResults = {"results" : results}; //ClassificationResults object.
+            return result;
+        }
+    }
+
+    export interface ClassificationResults {
+        results: Services.ClassificationResult[];
+    }
+}
 /**
   * This module serves as a bridge between the Java eclipse plugin and the TypeScript Services that are available.
   * An example diagram of what files talk to what looks like this.
@@ -268,68 +77,72 @@ module AutoComplete {
   */
 module TypeScriptServiceBridge {
 
+    /**
+     * All incoming objects must be IRequest objects.
+     */
     export interface IRequest {
         command: string; // the command
-        serviceType: string; // determines which service this message is for.
+        service: string; // determines which service this message is for.
+        args: any[]; // arguments
     }
 
-    export interface IResult {
-        resultValid: boolean;
-        resultType: string; // this should be the same as the command for the corresponding IResult.
-    }
-
-    export interface IError extends IResult {
-        errorMessage: string;
-    }
-
+    /**
+     * All Services must implement IService.
+     */
     export interface IService {
-        /*
-           The Service  is a wrapper around the built in typescript services to provide the functionality we want and respond to the right commands.
-        */
-        getServiceType(): string;
-        processRequest(request: IRequest): IResult;
+        getServiceName(): string;
+        getService(): any;
     }
 
     export class TSServiceBridge {
 
-        private services: IService[] = []; // For every service you want to call, you have to provide a server for it here.
+        private services: IService[] = [];
 
         constructor() {
             this.populateservices();
         }
 
         public static invalidResult(error: string) {
-            var result = { "resultValid": false, "resultType": "error", "errorMessage": error};
+            var result = {"error" : error};
             return result;
         }
 
-        private populateservices() { // This is how you add new Services.  Just push a new service onto this.services and it will be used.
-            this.services.push(new SyntaxHighlight.Service());
-            this.services.push(new AutoComplete.Service());
+        private populateservices() { // Add the services here.
+            this.services.push(new BridgeService.ClassifierService());
         }
 
         private invalidJSON() {
             return TSServiceBridge.invalidResult("invalid json");
         }
 
-        private invalidCommand() {
+        private invalidService() {
             return TSServiceBridge.invalidResult("invalid command");
         }
 
-        private processRequest(request) { // hands off the request to the appropriate IService
-            var serviceType = request.serviceType;
-            var result: IResult;
-            result = this.invalidCommand(); // default value of result is invalid.
+        private preProcessRequest(request) { // hands off the request to the appropriate IService
+            var serviceName = request.service;
+            var result;
+            result = this.invalidService(); // default value of result is invalid.
             for (var i=0; i<this.services.length; i++) {
-                if (serviceType === this.services[i].getServiceType()) {
-                    result = this.services[i].processRequest(request);
+                if (serviceName === this.services[i].getServiceName()) {
+                    result = this.processRequest(request,this.services[i].getService());
                     break;
                 }
             }
             return this.sendResult(result);
         }
 
-        private sendResult(result: IResult) {
+        private processRequest(request: IRequest, service: any) {
+            var command: string = request.command;
+            var args: any[] = request.args;
+            var methodInstance: any = service[command];
+            var rawResult: any = methodInstance.apply(service,args);
+            var resultValid: boolean = true;
+            var resultType: string = command;
+            return rawResult;
+        }
+
+        private sendResult(result) {
             var rawResult = JSON.stringify(result);
             console.log(rawResult);
             return;
@@ -344,7 +157,7 @@ module TypeScriptServiceBridge {
                 result = this.invalidJSON();
                 return this.sendResult(result);
             }
-            return this.processRequest(request);
+            return this.preProcessRequest(request);
         }
 
         public run() {
