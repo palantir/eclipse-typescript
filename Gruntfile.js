132d1d6932f8f6492e84c3f2608f2a73b61a3de6
Derek Cicerone <derekcicerone@gmail.com>
2013-07-13 13:57:06 -0400

    Random formatting and cleanup of Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,41 @@
-module.exports = function(grunt) {
+/*
+ * Copyright 2013 Palantir Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-  // Project configuration.
+module.exports = function(grunt) {
+  'use strict';
+
+  // project configuration
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     typescript: {
       compile: {
         options: {
-          module: "amd",
-          sourcemap: true,
-          target: "es5"
+          target: 'es5'
         },
-        src: ["Bridge/src/bridge.ts"],
-        dest: "com.palantir.typescript/bin/bridge.js"
+        src: ['Bridge/src/bridge.ts'],
+        dest: 'com.palantir.typescript/bin/bridge.js'
       }
     }
   });
 
-  //load NPM tasks
+  // load NPM tasks
   grunt.loadNpmTasks('grunt-typescript');
 
-  // Default task(s).
-  grunt.registerTask('default', ["typescript"]);
-  grunt.registerTask('travis', ["typescript"]);
-
+  // other tasks
+  grunt.registerTask('default', ['typescript']);
+  grunt.registerTask('travis', ['typescript']);
 };
