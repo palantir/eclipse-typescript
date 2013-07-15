1cfe73b7704f701790e068b0592988d56690ab09
Derek Cicerone <derekcicerone@gmail.com>
2013-07-15 11:40:52 -0400

    Switch to a design that is less clever but should be 100% reliable

--- com.palantir.typescript/src/com/palantir/typescript/text/TypeScriptDamageRepairer.java
+++ com.palantir.typescript/src/com/palantir/typescript/text/TypeScriptDamageRepairer.java
@@ -75,37 +75,20 @@ public final class TypeScriptDamageRepairer implements IPresentationDamager, IPr
     @Override
     public IRegion getDamageRegion(ITypedRegion partition, DocumentEvent event, boolean documentPartitioningChanged) {
         int offset = event.getOffset();
-        String text = event.getText();
-        int length = text != null ? text.length() : 0;
+        int documentLength = this.document.getLength();
 
-        this.damageFinalLexStates(event);
+        // remove all final lex states after the offset
+        Iterator<Entry<Integer, EndOfLineState>> it = this.finalLexStates.entrySet().iterator();
+        while (it.hasNext()) {
+            Entry<Integer, EndOfLineState> entry = it.next();
+            Integer entryOffset = entry.getKey();
 
-        // determine the damaged region
-        try {
-            IRegion startLineInfo = this.document.getLineInformationOfOffset(offset);
-            IRegion endLineInfo = this.document.getLineInformationOfOffset(offset + length);
-
-            int startOffset = startLineInfo.getOffset();
-            int endOffset = endLineInfo.getOffset() + endLineInfo.getLength();
-
-            // a region of length 0 doesn't work, so we need to damage the next line as well
-            if (startOffset == endOffset) {
-                int line = this.document.getLineOfOffset(endOffset);
-                String lineDelimiter = this.document.getLineDelimiter(line);
-
-                if (startOffset + lineDelimiter.length() < this.document.getLength()) {
-                    IRegion nextLineInfo = this.document.getLineInformation(line + 1);
-
-                    endOffset = nextLineInfo.getOffset() + nextLineInfo.getLength();
-                } else {
-                    endOffset = this.document.getLength();
-                }
+            if (entryOffset >= offset) {
+                it.remove();
             }
-
-            return new Region(startOffset, endOffset - startOffset);
-        } catch (BadLocationException e) {
-            throw new RuntimeException(e);
         }
+
+        return new Region(offset, documentLength - offset);
     }
 
     @Override
@@ -147,7 +130,6 @@ public final class TypeScriptDamageRepairer implements IPresentationDamager, IPr
             int lineOffset = this.document.getLineOffset(line);
             ClassificationResult result = results.get(i);
             EndOfLineState finalLexState = result.getFinalLexState();
-            EndOfLineState previousFinalLexState = this.finalLexStates.get(lineOffset);
 
             // add the style ranges for the classified text
             int currentOffset = lineOffset;
@@ -161,13 +143,6 @@ public final class TypeScriptDamageRepairer implements IPresentationDamager, IPr
 
             // store the new final lex state
             this.finalLexStates.put(lineOffset, finalLexState);
-
-            // re-classify the rest of the document if the last final lex state changed
-            if (i == results.size() - 1 && previousFinalLexState != finalLexState) {
-                int lastLine = this.document.getLineOfOffset(this.document.getLength());
-
-                this.classifyLines(presentation, endLine + 1, lastLine);
-            }
         }
     }
 
@@ -179,31 +154,6 @@ public final class TypeScriptDamageRepairer implements IPresentationDamager, IPr
         StyleRange styleRange = new StyleRange(offset, length, foreground, background, fontStyle);
 
         presentation.addStyleRange(styleRange);
-    }
-
-    private void damageFinalLexStates(DocumentEvent event) {
-        int offset = event.getOffset();
-        String text = event.getText();
-        int oldLength = event.getLength();
-        int newLength = text != null ? text.length() : 0;
-        int lengthDelta = newLength - oldLength;
-
-        Map<Integer, EndOfLineState> newFinalLexStates = Maps.newHashMap();
-        Iterator<Entry<Integer, EndOfLineState>> it = this.finalLexStates.entrySet().iterator();
-        while (it.hasNext()) {
-            Entry<Integer, EndOfLineState> entry = it.next();
-            Integer entryOffset = entry.getKey();
-
-            if (entryOffset > offset) {
-                it.remove();
-
-                if (entryOffset > offset + oldLength) {
-                    newFinalLexStates.put(entryOffset + lengthDelta, entry.getValue());
-                }
-            }
-        }
-
-        this.finalLexStates.putAll(newFinalLexStates);
     }
 
     private static ImmutableMap<TokenClass, TextAttribute> createClassificationTextAttributes(ColorManager manager) {
