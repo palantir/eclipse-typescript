f93acbdb5ddb218c24885b7a55e8ec7f4141d1e2
Riccardo Serafin <rserafin@tsbtecnologias.es>
2013-08-23 09:39:59 +0200

    Added reference to original copyright notice.

--- com.palantir.typescript/src/com/palantir/typescript/util/CharOperation.java
+++ com.palantir.typescript/src/com/palantir/typescript/util/CharOperation.java
@@ -18,6 +18,18 @@ package com.palantir.typescript.util;
 
 /**
  * Subset of org.eclipse.jdt.core.compiler.CharOperation used to support path pattern matching.
+ * <p>
+ * Original copyright notice:
+ * <p>
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Luiz-Otavio Zorzella <zorzella at gmail dot com> - Improve CamelCase algorithm
  *
  * @author rserafin
  */
