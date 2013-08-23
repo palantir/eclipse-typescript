355d0961f9aae30c87ad11d6e0d842ff6be60e07
tylera <tylera@palantir.com>
2013-08-22 21:21:33 -0400

    Regression even more fixed.  Also style fixes to PrefixMatcher

--- com.palantir.typescript/src/com/palantir/typescript/text/PrefixMatcher.java
+++ com.palantir.typescript/src/com/palantir/typescript/text/PrefixMatcher.java
@@ -29,31 +29,33 @@ import com.google.common.base.Ascii;
  */
 public final class PrefixMatcher {
 
+    private final Pattern camelCasePattern;
     private final Pattern lowerCasePattern;
-    private final Pattern pattern;
 
     public PrefixMatcher(String prefix) {
         checkNotNull(prefix);
 
         // turn the * wildcard into the regex .*
-        prefix = prefix.replaceAll("\\*", ".\\*");
-        this.lowerCasePattern = Pattern.compile("^" + prefix + ".*");
+        String dotStarPrefix = prefix.replaceAll("\\*", ".\\*");
 
-        if (!prefix.isEmpty()) {
+        String lowerCasePrefix = "^" + Ascii.toLowerCase(prefix) + ".*";
+
+        if (!dotStarPrefix.isEmpty()) {
             // the first character is treated special because we don't want to match lower
             // case characters before the first character if it is upper case
             // for example: ADL matches AddDefaultLibrary and NOT setAddDefaultLibrary
             // but aDL matches addDefaultLibrary
-            prefix = prefix.substring(0, 1) + prefix.substring(1).replaceAll("([A-Z])", "[a-z0-9]*$1");
+            dotStarPrefix = prefix.substring(0, 1) + prefix.substring(1).replaceAll("([A-Z])", "[a-z0-9]*$1");
         }
 
         // this match starts at the beginning of the string but doesn't need to go to the end
-        prefix = "^" + prefix + ".*";
+        String camelCasePrefix = "^" + dotStarPrefix + ".*";
 
-        this.pattern = Pattern.compile(prefix);
+        this.camelCasePattern = Pattern.compile(camelCasePrefix);
+        this.lowerCasePattern = Pattern.compile(lowerCasePrefix);
     }
 
     public boolean matches(String text) {
-        return this.pattern.matcher(text).matches() || this.lowerCasePattern.matcher(Ascii.toLowerCase(text)).matches();
+        return this.camelCasePattern.matcher(text).matches() || this.lowerCasePattern.matcher(Ascii.toLowerCase(text)).matches();
     }
 }
