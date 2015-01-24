45652f47bee6637d40ba87f91cafd3bae1314e17
Derek Cicerone <derekcicerone@gmail.com>
2015-01-24 12:58:59 -0500

    Fix locations in update script (and update lib.d.ts every time now)

--- scripts/updateTypeScript.sh
+++ scripts/updateTypeScript.sh
@@ -12,10 +12,13 @@ set -e
 )
 
 # copy the necessary bin files to build
-cp ../typescript/bin/lib.d.ts $(dirname $0)/../Bridge/bin/
-cp ../typescript/bin/tsc $(dirname $0)/../Bridge/bin/
-cp ../typescript/bin/tsc.js $(dirname $0)/../Bridge/bin/
+cp ../typescript/bin/lib.d.ts $(dirname $0)/../com.palantir.typescript/bridge/bin/
+cp ../typescript/bin/tsc $(dirname $0)/../com.palantir.typescript/bridge/bin/
+cp ../typescript/bin/tsc.js $(dirname $0)/../com.palantir.typescript/bridge/bin/
 
 # copy the language services JavaScript and definition file
-cp ../typescript/built/local/typescriptServices.js $(dirname $0)/../Bridge/lib/
-cp ../typescript/built/local/typescriptServices.d.ts $(dirname $0)/../Bridge/typings/
+cp ../typescript/built/local/typescriptServices.js $(dirname $0)/../com.palantir.typescript/bridge/lib/
+cp ../typescript/built/local/typescriptServices.d.ts $(dirname $0)/../com.palantir.typescript/bridge/typings/
+
+# copy the lib.d.ts definitions file into the plug-in
+cp ../typescript/bin/lib.d.ts $(dirname $0)/../com.palantir.typescript/src/com/palantir/typescript/services/language/
