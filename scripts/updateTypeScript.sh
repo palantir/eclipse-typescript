ec938dfa523cbab5929145b875b04dbe9c23dc38
sixinli <sixin210@hotmail.com>
2017-01-09 13:09:38 -0500

    update `TypeScript` casing

--- scripts/updateTypeScript.sh
+++ scripts/updateTypeScript.sh
@@ -13,17 +13,17 @@ set -e
 )
 
 # copy the necessary bin files to build
-cp ../Typescript/bin/tsc $(dirname $0)/../com.palantir.typescript/bridge/bin/
-cp ../Typescript/built/local/lib.d.ts $(dirname $0)/../com.palantir.typescript/bridge/lib/
-cp ../Typescript/built/local/tsc.js $(dirname $0)/../com.palantir.typescript/bridge/lib/
+cp ../TypeScript/bin/tsc $(dirname $0)/../com.palantir.typescript/bridge/bin/
+cp ../TypeScript/built/local/lib.d.ts $(dirname $0)/../com.palantir.typescript/bridge/lib/
+cp ../TypeScript/built/local/tsc.js $(dirname $0)/../com.palantir.typescript/bridge/lib/
 
 # copy the language services JavaScript and definition file
-cp ../Typescript/built/local/typescriptServices.js $(dirname $0)/../com.palantir.typescript/bridge/lib/
-cp ../Typescript/built/local/typescriptServices.d.ts $(dirname $0)/../com.palantir.typescript/bridge/typings/
+cp ../TypeScript/built/local/typescriptServices.js $(dirname $0)/../com.palantir.typescript/bridge/lib/
+cp ../TypeScript/built/local/typescriptServices.d.ts $(dirname $0)/../com.palantir.typescript/bridge/typings/
 
 # copy the default lib files into the plug-in
-cp ../Typescript/built/local/lib.d.ts $(dirname $0)/../com.palantir.typescript/src/com/palantir/typescript/services/language/
-cp ../Typescript/built/local/lib.es6.d.ts $(dirname $0)/../com.palantir.typescript/src/com/palantir/typescript/services/language/
+cp ../TypeScript/built/local/lib.d.ts $(dirname $0)/../com.palantir.typescript/src/com/palantir/typescript/services/language/
+cp ../TypeScript/built/local/lib.es6.d.ts $(dirname $0)/../com.palantir.typescript/src/com/palantir/typescript/services/language/
 
 # convert to unix line endings
 dos2unix $(dirname $0)/../com.palantir.typescript/bridge/lib/lib.d.ts
